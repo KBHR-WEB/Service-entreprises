d0b95f95d98a1876b90d2b7e5185820edfcaa2c0
KBHR-WEB <147394940+KBHR-WEB@users.noreply.github.com>
2023-11-27 12:13:37 +0000

    Create scripted.js

---
+++ scripted.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    var sideMenu = document.querySelector('.side-menu');
+    sideMenu.style.width = sideMenu.style.width === '75px' ? '0' : '75px';
+  }
+  
+  
+function closeMenu() {
+    var sideMenu = document.querySelector('.side-menu');
+    sideMenu.style.width = '0';
+  }
+  
